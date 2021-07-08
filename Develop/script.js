c33d3d8bf08bdb99f9c80248f7624bfb4a8771d1
Caroline <Cluce059@gmail.com>
2021-07-08 19:46:18 -0400

    working on the prompt logic

--- Develop/script.js
+++ Develop/script.js
@@ -17,12 +17,13 @@ var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var upperCaseQuestion = function(){
   var upperCaseConfirm = confirm("Do you want to include uppercase characters?");
   if(upperCaseConfirm){
-    finalPassword += uppercase; //ask about this to confirm but since strings are global this should change the finalPassword[] string
+    finalPassword += upperCase; //ask about this to confirm but since strings are global this should change the finalPassword[] string
     return finalPassword;//not sure how to use it yet so leaving these two return stmnts here for now
   }
   else{ //if no, finalpassword remains as is
     finalPassword += finalPassword;
   }
+  return;
 };
 
 //asks user if they want to include lowercase chars
@@ -35,34 +36,89 @@ var lowerCaseQuestion = function(){
   else{
     finalPassword += finalPassword;
   }
+  return;
 };
 
 //asks user if they want to include special chars
 var specialCharQuestion = function(){
-  var specialCharconfirm = window.confirm("Do you want to include special characters?");
+  var specialCharconfirm = confirm("Do you want to include special characters?");
   if(specialCharconfirm){
     finalPassword += specialChars;
   }
   else{
     finalPassword += finalPassword;
   }
+  return;
 };
 
 //asks user if they want to include numbers
 var numericCharsQuestion = function(){
-  var numericCharsConfirm = ("Do you wan to include numbers?");
+  var numericCharsConfirm = confirm("Do you wan to include numbers?");
   if(numericCharsConfirm){
     finalPassword += numericChars;
   }
   else{
     finalPassword += finalPassword;
   }
+  return;
 };
 
+//asks user how long they want password to be
+var passwordLengthQuestion = function(){
+  var passwordLength = prompt("how long should your password be? Must be between 8-128 characters");
+  passwordLength = finalPassword.length;
+    //logic to change length to user input
+};
+
+
+
+
+
 var generatePassword = function(){
-  //window prompts getting user input to pass to function
-  criteriaPrompt();
-  //call the prompts
+
+   
+  //uppercase question
+  var upperCaseConfirm = confirm("Do you want to include uppercase characters?");
+  if(upperCaseConfirm){
+    finalPassword += upperCase; //ask about this to confirm but since strings are global this should change the finalPassword[] string
+  }
+  else{ //if no, finalpassword remains as is
+    finalPassword += finalPassword;
+  }
+  //lowercase question
+  var lowerCaseConfirm = confirm("Do you want to include lowercase characters?");
+  if(lowerCaseConfirm){
+    finalPassword += lowerCase; //now to find out how to use math.random to select a certain number of chars to include based on user input... by using the index maybe?
+  }
+  else{
+    finalPassword += finalPassword;
+  }
+  //special char question
+  var specialCharconfirm = confirm("Do you want to include special characters?");
+  if(specialCharconfirm){
+    finalPassword += specialChars;
+  }
+  else{
+    finalPassword += finalPassword;
+  }
+  //numeric question
+  var numericCharsConfirm = confirm("Do you wan to include numbers?");
+  if(numericCharsConfirm){
+    finalPassword += numericChars;
+  }
+  else{
+    finalPassword += finalPassword;
+  }
+     //length question
+     var passwordLength = prompt("how long should your password be? Must be between 8-128 characters");
+     passwordLength = parseInt(passwordLength, 8-128);
+       if(passwordLength >= 8 && passwordLength <= 128){
+       passwordLength = finalPassword.length;     
+      }
+     else{
+       alert("you need to enter a valid length");
+       return generatePassword();
+     }
 };
 //obv u should use math.random!!!ask skeletoning it first tho
 
