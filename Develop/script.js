0f7f8dbd516213ce494b17067a2dc14a1e4f455d
Caroline <Cluce059@gmail.com>
2021-07-10 11:24:03 -0400

    password generation

--- Develop/script.js
+++ Develop/script.js
@@ -10,49 +10,14 @@
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", " y", "z"]; 
 var specialChars = ["!", "@", "#", "$", "^", "&", "&", "*", "(", ")"];
-var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-
-
-// var upperCaseQuestion = function(){
-
-//   return;
-// };
-
-// //asks user if they want to include lowercase chars
-// var lowerCaseQuestion = function(){
-
-// };
-
-// //asks user if they want to include special chars
-// var specialCharQuestion = function(){
-
-//   return;
-// };
-
-// //asks user if they want to include numbers
-// var numericCharsQuestion = function(){
-
-//   return;
-// };
-
-// //asks user how long they want password to be
-// var passwordLengthQuestion = function(){
-//   var passwordLength = prompt("how long should your password be? Must be between 8-128 characters");
-//   passwordLength = parseInt(passwordLength);
-//   if(passwordLength < 8 || passwordLength > 128){
-//     alert("you must chose a length between 8-128");
-//     return false;
-//   }
-//   return passwordLength;
-// };
+var numericChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //generate password function
 var generatePassword = function(){
-  var finalPassword = [];
+  var loadPassword = [];
   //or//finalPassword = "";
   // const random = Math.floor(Math.random() * upperCase.length);
-  // console.log(upperCase[random]);
+  
   //get user input for length
   //finalPassword.length = passwordLengthQuestion();
   var passwordLength = prompt("how long should your password be? Must be between 8-128 characters");
@@ -64,49 +29,49 @@ var generatePassword = function(){
   //does user want to include ints?
   var numericCharsConfirm = confirm("Do you wan to include numbers?");
   if(numericCharsConfirm){
-    finalPassword += numericChars;
+    loadPassword = loadPassword.concat(numericChars);
   }
-
-
+  //join() makes arr to string
   //does user want lowercase chars?
   var lowerCaseConfirm = confirm("Do you want to include lowercase characters?");
   if(lowerCaseConfirm){
-    finalPassword += lowerCase; //now to find out how to use math.random to select a certain number of chars to include based on user input... by using the index maybe?
+    loadPassword = loadPassword.concat(lowerCase); //now to find out how to use math.random to select a certain number of chars to include based on user input... by using the index maybe?
   }
 
   //does user want uppercase chars?
   var upperCaseConfirm = confirm("Do you want to include uppercase characters?");
   if(upperCaseConfirm){
-    finalPassword += upperCase; //ask about this to confirm but since strings are global this should change the finalPassword[] string
+    loadPassword = loadPassword.concat(upperCase); //ask about this to confirm but since strings are global this should change the finalPassword[] string
   }
 
   //does user want special chars?
   var specialCharconfirm = confirm("Do you want to include special characters?");
   if(specialCharconfirm){
-    finalPassword += specialChars;
+    loadPassword = loadPassword.concat(specialChars);
   }
 
   //math.random logic
-  const genPassword = [];
+  var finalPassword = "";
+  //finalPassword.length = passwordLength;
+  console.log(typeof(loadPassword));
+  
   for(var i = 0; i < passwordLength; i++){
-    genPassword[i] += finalPassword[Math.floor(Math.random()*finalPassword.length)];
+    // var index, password;
+    // index = Math.floor(Math.random()*loadPassword.length);
+    //password = loadPassword[index];
+    finalPassword += loadPassword[Math.floor(Math.random()*loadPassword.length)];
+ 
+    // console.log(index);
+    // console.log(password);
+    //
   }
+ //console.log(loadPassword);
 console.log(finalPassword);
-//maybe here start the math.random() and geta results of passwordlength length arr?
+//maybe here start the math.random() and get results of passwordlength length arr?
 
 return finalPassword;
 };
 //obv u should use math.random!!!ask skeletoning it first tho
-
-
-
-
-
-
-
-
-
-
 
 //////////////////////Dont write past me! :]///////////////////////////////////////////////////////////////////////////////
 // Get references to the #generate element
